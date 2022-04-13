425f0808c0b773917975cd252fa91dbbf6a9a2dd
ifeelpankaj <91965663+ifeelpankaj@users.noreply.github.com>
2022-04-13 07:50:11 +0530

    Final

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,39 +1,40 @@
 
 
+import logo from "../../images/BuyMyBook.png"
 import React from "react";
 import { ReactNavbar } from "overlay-navbar";
-import logo from "../../images/BuyMyBook.png"
+import logo from "../../../images/logo.png";
 
 const options = {
-  burgerColorHover: "#0c5452",
+  burgerColorHover: "#eb4034",
   logo,
-  logoWidth: "200px",
-  navColor1: "#0c5452",
+  logoWidth: "20vmax",
+  navColor1: "white",
   logoHoverSize: "10px",
   logoHoverColor: "#eb4034",
   link1Text: "Home",
   link2Text: "Products",
   link3Text: "Contact",
   link4Text: "About",
-  link1Url: "/home",
+  link1Url: "/",
   link2Url: "/products",
   link3Url: "/contact",
   link4Url: "/about",
   link1Size: "1.3vmax",
-  link1Color: "White",
+  link1Color: "rgba(35, 35, 35,0.8)",
   nav1justifyContent: "flex-end",
   nav2justifyContent: "flex-end",
   nav3justifyContent: "flex-start",
   nav4justifyContent: "flex-start",
-  link1ColorHover: "yellow",
+  link1ColorHover: "#eb4034",
   link1Margin: "1vmax",
   profileIconUrl: "/login",
-  profileIconColor: "white",
-  searchIconColor: "white",
-  cartIconColor: "white",
-  profileIconColorHover: "yellow",
-  searchIconColorHover: "yellow",
-  cartIconColorHover: "yellow",
+  profileIconColor: "rgba(35, 35, 35,0.8)",
+  searchIconColor: "rgba(35, 35, 35,0.8)",
+  cartIconColor: "rgba(35, 35, 35,0.8)",
+  profileIconColorHover: "#eb4034",
+  searchIconColorHover: "#eb4034",
+  cartIconColorHover: "#eb4034",
   cartIconMargin: "1vmax",
 };
 
